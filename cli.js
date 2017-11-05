2a9c10ca41138d27ad3e14fbb70aff64837d71fb
Jannis R <mail@jannisr.de>
2017-11-05 18:44:20 +0100

    CLI interface, part 1

--- cli.js
+++ cli.js
@@ -1,4 +1,69 @@
 #!/usr/bin/env node
 'use strict'
 
+const mri = require('mri')
+
+const pkg = require('./package.json')
+const createFilter = require('./lib/create-filter')
+
+const argv = mri(process.argv.slice(2), {
+	boolean: ['help', 'h', 'version', 'v']
+})
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    db-stations [options] [filters]
+
+Options:
+    --id         <value>              Filter by id.
+    --name       <value>              Filter by name.
+    --latitude   <value>              Filter by latitude.
+    --longitude  <value>              Filter by longitude.
+    --weight     <value>              Filter by weight.
+    --format     <csv|ndjson>         Default is ndjson.
+    --columns    <value>,<value>,…    Default is id,coords,weight,name.
+
+Filters:
+    Each filter must be an \`Array.prototype.filter\`-compatible funtion.
+
+Examples:
+    db-stations
+    db-stations --name 'elfershausen trimberg'
+    db-stations --id 8005229 --columns id,name,weight
+    db-stations "(s) => s.latitude > 53" "(s) => s.latitude > 12"
+\n`)
+	process.exit()
+}
+
+const showError = (err) => {
+	console.error(err.message || (err + ''))
+	process.exit(1)
+}
+
+
+
+let selector = Object.create(null)
+if (Object.keys(selector).length === 0) selector = 'all'
+else {
+	if (argv.id) selector.id = argv.id.toString().trim()
+	if ('latitude' in argv) selector.latitude = parseFloat(argv.latitude)
+	if ('longitude' in argv) selector.longitude = parseFloat(argv.longitude)
+	if ('weight' in argv) selector.weight = parseFloat(argv.weight)
+}
+
+const filters = [
+	filterStream.obj(createFilter(selector))
+]
+for (let i = 0; i < argv._; i++) {
+	const code = argv._[i]
+
+	try {
+		const filterFn = new Function(code)
+		filters.push(filterStream.obj(filterFn))
+	} catch (err) {
+		showError(`Argument ${i} is not a valid JS function.`)
+	}
+}
+
 // todo
